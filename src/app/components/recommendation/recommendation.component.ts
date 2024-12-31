2caf705b461bce9bae985e86e9d7c67a785cfcb1
Dipak Dhuri <dipakdhurimlnn@gmail.com>
2024-12-31 12:29:52 +0530

    fixed routerlink

--- src/app/components/recommendation/recommendation.component.ts
+++ src/app/components/recommendation/recommendation.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-recommendation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './recommendation.component.html',
   styleUrl: './recommendation.component.css'
 })
