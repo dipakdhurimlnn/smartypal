d3cb198efec50ac1f25933c230ffeb2b69c7d2d4
Dipak Dhuri <dipakdhurimlnn@gmail.com>
2024-12-31 12:22:36 +0530

    second commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {AboutComponent} from './components/about/about.component';
+import {ProductsComponent} from './components/products/products.component';
+import {RecommendationComponent} from './components/recommendation/recommendation.component';
+import {ContactComponent} from './components/contact/contact.component';
+import {HomeComponent} from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'recommendation', component: RecommendationComponent},
+  {path: 'contact', component: ContactComponent},];
---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,29 @@
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-products',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.css'
+})
+export class ProductsComponent {
+  toys = [
+    {
+      name: 'Creative Building Blocks',
+      description: 'Encourage imagination and problem-solving with these colorful blocks.',
+      image: 'toy1.jpg'
+    },
+    {
+      name: 'STEM Robotics Kit',
+      description: 'Introduce coding and robotics to kids in a fun and engaging way.',
+      image: 'toy2.jpg'
+    },
+    {
+      name: 'Puzzle Adventures',
+      description: 'Boost cognitive skills with puzzles designed for curious minds.',
+      image: 'toy3.jpg'
+    }
+  ];
+}
