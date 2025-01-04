3d8e6b2625cadcab82aa931694b9034f77986b4e
Dipak Dhuri <dipakdhurimlnn@gmail.com>
2025-01-04 16:59:32 +0530

    update gitingore

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type {CapacitorConfig} from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.smartypal',
+  appName: 'smartypal',
+  webDir: 'dist'
+};
+
+export default config;
