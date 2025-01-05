730a58d6beddb8dd58d46998798dc19bc79f0ccc
Dipak Dhuri <dipakdhurimlnn@gmail.com>
2025-01-05 11:30:58 +0530

    added whatsapp chat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {CommonModule} from '@angular/common';
+import {WhatsappButtonComponent} from './shared/whatsapp-button/whatsapp-button.component';
 
 @Component({
   selector: 'app-root',
